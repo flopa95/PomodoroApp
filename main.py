4adf886fc301010c10fc587b0d8ead47bf568cb9
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-07-14 19:45:10 +0800

    fixed message

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ for i in range(1):
 
     time.sleep(300)
     winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Go.wav', winsound.SND_ALIAS)
-    print ('Your 25 minute break is over! Get back to work :)')
+    print ('Your 5 minute break is over! Get back to work :)')
 
 
 input()
