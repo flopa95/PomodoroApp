69d6a039e1c8a9ad46950b675011af47ae341963
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-07-14 19:11:26 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import winsound, time
+
+for i in range(1):
+
+    winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Go.wav', winsound.SND_ALIAS)
+
+    time.sleep(1500)
+    winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Over.wav', winsound.SND_ALIAS)
+    print ('Your 25 minute study period is over!')
+
+    time.sleep(300)
+    winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Go.wav', winsound.SND_ALIAS)
+    print ('Your 25 minute break is over!')
