0b0dbf935bf5dd57d473af37040b7f3c5ff63cc5
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-07-16 20:34:15 +0800

    Including Rectangle GUI

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-import winsound, time
+import winsound, time, tkinter
+
+popup = tkinter.Tk()
+popup.overrideredirect(1)
+popup.attributes('-topmost', True)
+
+screen_width = popup.winfo_screenwidth()
+popup.geometry('15x15+%d+0' % (screen_width/2))
+
+canvas = tkinter.Canvas(popup, width = 15, height = 15, background = "red",highlightthickness=0)
 
 for i in range(1):
 
+    canvas.configure(background = "red")
+    canvas.pack()
+    popup.update()
     winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Go.wav', winsound.SND_ALIAS)
     print ('Time to be productive! Seek discomfort.')
 
     time.sleep(1500)
+    canvas.configure(background = "green")
+    canvas.pack()
+    popup.update()
     winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Over.wav', winsound.SND_ALIAS)
     print ('Your 25 minute study period is over! Take a break, you earned it!')
 
     time.sleep(300)
-    winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Go.wav', winsound.SND_ALIAS)
-    print ('Your 5 minute break is over! Get back to work :)')
+    
 
 
 input()
+popup.mainloop()
