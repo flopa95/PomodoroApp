ca581b120190925d531c5523baa93d8d85369af5
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-07-17 19:11:16 +0800

    changed the file directory

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import winsound, time, tkinter
 popup = tkinter.Tk()
 popup.overrideredirect(1)
 popup.attributes('-topmost', True)
+#popup.attributes('-alpha',0.3)
 
 screen_width = popup.winfo_screenwidth()
 popup.geometry('15x15+%d+0' % (screen_width/2))
@@ -14,19 +15,17 @@ for i in range(1):
     canvas.configure(background = "red")
     canvas.pack()
     popup.update()
-    winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Go.wav', winsound.SND_ALIAS)
+    winsound.PlaySound('Pomodoro_Go.wav', winsound.SND_ALIAS)
     print ('Time to be productive! Seek discomfort.')
 
     time.sleep(1500)
     canvas.configure(background = "green")
     canvas.pack()
     popup.update()
-    winsound.PlaySound('C:/Users/User/Documents/GitHub/PomodoroApp/Pomodoro_Over.wav', winsound.SND_ALIAS)
+    winsound.PlaySound('Pomodoro_Over.wav', winsound.SND_ALIAS)
     print ('Your 25 minute study period is over! Take a break, you earned it!')
 
     time.sleep(300)
     
-
-
 input()
 popup.mainloop()
